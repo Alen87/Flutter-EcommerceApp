c4dba438de1bf59c404efdf78e3c5cfced74ddf1
Alen Boric <boricalen@live.com>
2023-01-31 22:40:19 +0100

    BottomNavigationBar

--- multi_store/lib/views/buyers/main_screen.dart
+++ multi_store/lib/views/buyers/main_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -11,8 +13,35 @@ class _MainScreen extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Main Screen'),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.yellow.shade900,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'HOME',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/explore.svg', width: 20),
+            label: 'CATEGORIES',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/shop.svg', width: 20),
+            label: 'STORE',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/cart.svg'),
+            label: 'CART',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/search.svg'),
+            label: 'SEARCH',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/account.svg'),
+            label: 'ACCOUNT',
+          ),
+        ],
       ),
     );
   }
