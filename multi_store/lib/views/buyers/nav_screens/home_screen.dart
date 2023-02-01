a3c9adafff8492621cb60357fb39d2a7c9445f3f
Alen Boric <boricalen@live.com>
2023-02-01 23:22:39 +0100

    HomeScreen update

--- multi_store/lib/views/buyers/nav_screens/home_screen.dart
+++ multi_store/lib/views/buyers/nav_screens/home_screen.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home screen'),
+    return Padding(
+      padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top, left: 25, right: 15),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                'Howdy ,What are You\n Looking For 👀',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                child: SvgPicture.asset(
+                  'assets/icons/cart.svg',
+                  width: 20,
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 14,
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: TextField(
+              decoration: InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                hintText: 'Search For Products',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                ),
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: SvgPicture.asset(
+                    'assets/icons/search.svg',
+                    width: 10,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
