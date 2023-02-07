a08e1c393dffd1be5724f3dff08be12efa439f58
Alen Boric <boricalen@live.com>
2023-02-08 00:58:56 +0100

    Start CategoriesScreen

--- multi_web_admin/lib/views/screens/side_bar_screens/categories_screen.dart
+++ multi_web_admin/lib/views/screens/side_bar_screens/categories_screen.dart
@@ -6,16 +6,75 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        alignment: Alignment.topLeft,
-        padding: const EdgeInsets.all(10),
-        child: const Text(
-          'Categories',
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 36,
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Category',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 36,
+              ),
+            ),
           ),
-        ),
+          Divider(
+            color: Colors.grey,
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 140,
+                      width: 140,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade500,
+                        border: Border.all(color: Colors.grey.shade800),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text('Category'),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.yellow.shade900),
+                      onPressed: () {},
+                      child: Text('Upload Image'),
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                child: SizedBox(
+                  width: 180,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter Category Name',
+                      hintText: 'Enter Category Name',
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                style:
+                    ElevatedButton.styleFrom(primary: Colors.yellow.shade900),
+                onPressed: () {},
+                child: Text('Save'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
