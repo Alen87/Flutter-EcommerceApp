43a2444c0ea330cd8c24057f0ce0b286e4d2007a
Alen Boric <boricalen@live.com>
2023-02-07 02:12:27 +0100

    initializeApp

--- multi_web_admin/lib/main.dart
+++ multi_web_admin/lib/main.dart
@@ -1,29 +1,34 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:multi_web_admin/views/screens/main_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: kIsWeb || Platform.isAndroid
+          ? FirebaseOptions(
+              apiKey: "AIzaSyCWYF-yBhmOjhLT96NnKVqUawfXbZjmkEc",
+              appId: "1:614719216012:web:f9f8a41fffa12e031e9ea9",
+              messagingSenderId: "614719216012",
+              projectId: "multi-store-aa6ce",
+              storageBucket: "multi-store-aa6ce.appspot.com",
+            )
+          : null);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MainScreen(),
