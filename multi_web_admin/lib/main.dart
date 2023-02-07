9ca892c9809259da34a6a5665a8c82f84cfa282f
Alen Boric <boricalen@live.com>
2023-02-08 00:37:03 +0100

    EasyLoading

--- multi_web_admin/lib/main.dart
+++ multi_web_admin/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:multi_web_admin/views/screens/main_screen.dart';
 
 void main() async {
@@ -32,6 +33,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MainScreen(),
+      builder: EasyLoading.init(),
     );
   }
 }
--- multi_web_admin/lib/views/screens/side_bar_screens/upload_banner_screen.dart
+++ multi_web_admin/lib/views/screens/side_bar_screens/upload_banner_screen.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class UploadBannerScreen extends StatefulWidget {
   static const String routeName = '\UploadBannerScreen';
@@ -40,10 +41,13 @@ class _UploadBannerScreenState extends State<UploadBannerScreen> {
   }
 
   uploadToFireStore() async {
+    EasyLoading.show();
     if (_image != null) {
       String imageUrl = await _uploadBannersToStorage(_image);
       await _firestore.collection('banners').doc(fileName).set({
         'image': imageUrl,
+      }).whenComplete(() {
+        EasyLoading.dismiss();
       });
     }
   }
