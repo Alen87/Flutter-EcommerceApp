cd2922786d9dac941fbc842b499b3ca32dde2f23
Alen Boric <boricalen@live.com>
2023-02-08 01:20:54 +0100

    Validator Check

--- multi_web_admin/lib/views/screens/side_bar_screens/categories_screen.dart
+++ multi_web_admin/lib/views/screens/side_bar_screens/categories_screen.dart
@@ -3,78 +3,97 @@ import 'package:flutter/material.dart';
 class CategoriesScreen extends StatelessWidget {
   static const String routeName = '\CategoriesScreen';
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  uploadCategory() {
+    if (_formKey.currentState!.validate()) {
+      print('Good');
+    } else {
+      print('Bad');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          Container(
-            alignment: Alignment.topLeft,
-            padding: const EdgeInsets.all(10),
-            child: const Text(
-              'Category',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 36,
-              ),
-            ),
-          ),
-          Divider(
-            color: Colors.grey,
-          ),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(14.0),
-                child: Column(
-                  children: [
-                    Container(
-                      height: 140,
-                      width: 140,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade500,
-                        border: Border.all(color: Colors.grey.shade800),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                        child: Text('Category'),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.yellow.shade900),
-                      onPressed: () {},
-                      child: Text('Upload Image'),
-                    ),
-                  ],
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.topLeft,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                'Category',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 36,
                 ),
               ),
-              Flexible(
-                child: SizedBox(
-                  width: 180,
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Enter Category Name',
-                      hintText: 'Enter Category Name',
+            ),
+            Divider(
+              color: Colors.grey,
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 140,
+                        width: 140,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade500,
+                          border: Border.all(color: Colors.grey.shade800),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text('Category'),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.yellow.shade900),
+                        onPressed: () {},
+                        child: Text('Upload Image'),
+                      ),
+                    ],
+                  ),
+                ),
+                Flexible(
+                  child: SizedBox(
+                    width: 180,
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please Category Name Must not be empty';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Enter Category Name',
+                        hintText: 'Enter Category Name',
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              ElevatedButton(
-                style:
-                    ElevatedButton.styleFrom(primary: Colors.yellow.shade900),
-                onPressed: () {},
-                child: Text('Save'),
-              ),
-            ],
-          ),
-        ],
+                SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(primary: Colors.yellow.shade900),
+                  onPressed: uploadCategory,
+                  child: Text('Save'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
