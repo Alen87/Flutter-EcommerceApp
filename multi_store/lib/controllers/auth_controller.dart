d771c5c7e30b9ce10b14fb633b2668f595cfce64
Alen Boric <boricalen@live.com>
2023-02-10 12:36:42 +0100

    AuthController class

---
+++ multi_store/lib/controllers/auth_controller.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthController {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<String> signUpUsers(String email, String fullName, String phoneNumber,
+      String password) async {
+    String res = 'Some error occurred';
+
+    try {
+      if (email.isNotEmpty &&
+          fullName.isNotEmpty &&
+          phoneNumber.isNotEmpty &&
+          password.isNotEmpty) {
+        // Create user
+
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+      } else {
+        res = 'Fields should not be empty.';
+      }
+    } catch (e) {}
+    return res;
+  }
+}
