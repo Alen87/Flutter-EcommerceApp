762876ff592412a09f087649df9b88276bd47250
Alen Boric <boricalen@live.com>
2023-02-12 23:25:27 +0100

    Save Users

--- multi_store/lib/controllers/auth_controller.dart
+++ multi_store/lib/controllers/auth_controller.dart
@@ -1,7 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthController {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<String> signUpUsers(String email, String fullName, String phoneNumber,
       String password) async {
@@ -12,11 +14,18 @@ class AuthController {
           fullName.isNotEmpty &&
           phoneNumber.isNotEmpty &&
           password.isNotEmpty) {
-        // Create user
+        // Create the user
 
         UserCredential cred = await _auth.createUserWithEmailAndPassword(
             email: email, password: password);
-        res = 'success';
+
+        await _firestore.collection('buyers').doc(cred.user!.uid).set({
+          'email': email,
+          'fulName': fullName,
+          'phoneNumber': phoneNumber,
+        });
+
+        res = 'Success';
       } else {
         res = 'Fields should not be empty.';
       }
