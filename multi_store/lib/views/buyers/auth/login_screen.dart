fbed63e76bc905b5d0a3266879d9ba484d855922
Alen Boric <boricalen@live.com>
2023-02-14 23:12:58 +0100

    Login Users

--- multi_store/lib/views/buyers/auth/login_screen.dart
+++ multi_store/lib/views/buyers/auth/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:multi_store/controllers/auth_controller.dart';
+import 'package:multi_store/utils/show_snackBar.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -6,67 +8,98 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final AuthController _authController = AuthController();
   late String email;
 
   late String password;
+
+  _loginUsers() async {
+    if (_formKey.currentState!.validate()) {
+      await _authController.loginUsers(email, password);
+      return showSnack(context, 'You Are Now Log In');
+    } else {
+      return showSnack(context, 'The Fields Should Not Be Empty.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Login Customer`s Account',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(13.0),
-              child: TextFormField(
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration: InputDecoration(labelText: 'Enter Email Address'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(13.0),
-              child: TextFormField(
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: InputDecoration(labelText: 'Enter Password'),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width - 40,
-              height: 50,
-              decoration: BoxDecoration(
-                color: Colors.yellow.shade900,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Center(
-                child: Text(
-                  'Login',
-                  style: TextStyle(letterSpacing: 5, color: Colors.white),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Login Customer`s Account',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Need An Account? '),
-                TextButton(onPressed: () {}, child: Text('Register')),
-              ],
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.all(13.0),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'The Email Field Should Not Be Empty.';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: InputDecoration(labelText: 'Enter Email Address'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(13.0),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'The Password Field Should Not Be Empty.';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: InputDecoration(labelText: 'Enter Password'),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              InkWell(
+                onTap: _loginUsers,
+                child: Container(
+                  width: MediaQuery.of(context).size.width - 40,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.yellow.shade900,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Login',
+                      style: TextStyle(letterSpacing: 5, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Need An Account? '),
+                  TextButton(onPressed: () {}, child: Text('Register')),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
