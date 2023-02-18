103afdae7d64d8ebac26073adfdc2e6a248cfc8e
Alen Boric <boricalen@live.com>
2023-02-18 14:49:49 +0100

    Register Vendor

---
+++ vendor_only/lib/vendor/views/auth/vendor_register_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class VendorRegistrationScreen extends StatelessWidget {
+  const VendorRegistrationScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Authenticated'),
+      ),
+    );
+  }
+}
