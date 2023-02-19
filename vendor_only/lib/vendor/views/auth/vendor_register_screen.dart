d6c019ab1e5115847d7c7aee51a6261999f63e8f
Alen Boric <boricalen@live.com>
2023-02-19 17:02:08 +0100

    LayoutBuilder

--- vendor_only/lib/vendor/views/auth/vendor_register_screen.dart
+++ vendor_only/lib/vendor/views/auth/vendor_register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class VendorRegistrationScreen extends StatelessWidget {
@@ -7,12 +8,48 @@ class VendorRegistrationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          toolbarHeight: 200,
-        ),
-      ],
-    ));
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            toolbarHeight: 200,
+            flexibleSpace: LayoutBuilder(builder: (context, constraints) {
+              return FlexibleSpaceBar(
+                background: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.yellow.shade900,
+                        Colors.yellow,
+                      ],
+                    ),
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 90,
+                          width: 90,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(
+                              10,
+                            ),
+                          ),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(CupertinoIcons.photo),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }),
+          ),
+        ],
+      ),
+    );
   }
 }
