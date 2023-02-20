5ba4f8de7e208934e3358372478dee0fd8321453
Alen Boric <boricalen@live.com>
2023-02-21 00:37:35 +0100

    Save Vendor Details

--- vendor_only/lib/vendor/controllers/vendor_register_controller.dart
+++ vendor_only/lib/vendor/controllers/vendor_register_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:image_picker/image_picker.dart';
 
 class VendorController {
@@ -14,4 +16,37 @@ class VendorController {
   }
 
   // FUNCTION TO PICK STORE IMAGE ENDS HERE
+
+  String registerVendor(
+    String businessName,
+    String email,
+    String phoneNumber,
+    String countryValue,
+    String stateValue,
+    String cityValue,
+    String taxOptions,
+    String taxNumber,
+    Uint8List? image,
+  ) {
+    String res = 'Some Error Occured';
+    try {
+      if (businessName.isNotEmpty &&
+          email.isNotEmpty &&
+          phoneNumber.isNotEmpty &&
+          countryValue.isNotEmpty &&
+          stateValue.isNotEmpty &&
+          cityValue.isNotEmpty &&
+          taxOptions.isNotEmpty &&
+          taxNumber.isNotEmpty &&
+          image != null) {
+        // SAVE DATA TO CLOUD FIRESTORE
+      } else {
+        res = 'Fields Should Not Be Empty.';
+      }
+      ;
+    } catch (e) {
+      res = e.toString();
+    }
+    return res;
+  }
 }
