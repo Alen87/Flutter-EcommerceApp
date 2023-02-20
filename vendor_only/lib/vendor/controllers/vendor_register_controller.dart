dcfe530b89e0906c7e0c58a737af5ae128b2027a
Alen Boric <boricalen@live.com>
2023-02-21 00:53:16 +0100

    Save Store Image

--- vendor_only/lib/vendor/controllers/vendor_register_controller.dart
+++ vendor_only/lib/vendor/controllers/vendor_register_controller.dart
@@ -1,8 +1,27 @@
 import 'dart:typed_data';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 
 class VendorController {
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // FUNCTION TO STORE IMAGE IN FIREBASE STORAGE
+
+  _uploadVendorImageToStorage(Uint8List? image) async {
+    Reference ref =
+        _storage.ref().child('storeImages').child(_auth.currentUser!.uid);
+
+    UploadTask uploadTask = ref.putData(image!);
+    TaskSnapshot snapshot = await uploadTask;
+    String downloadUrl = await snapshot.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  //FUNCTION TO STORE IMAGE IN FIREBASE STORAGE ENDS HERE
+
   // FUNCTION TO PICK STORE IMAGE
   pickStoreImage(ImageSource source) async {
     final ImagePicker _imagePicker = ImagePicker();
@@ -17,7 +36,8 @@ class VendorController {
 
   // FUNCTION TO PICK STORE IMAGE ENDS HERE
 
-  String registerVendor(
+  // FUNCTION TO SAVE VENDOR DATA
+  Future<String> registerVendor(
     String businessName,
     String email,
     String phoneNumber,
@@ -27,7 +47,7 @@ class VendorController {
     String taxOptions,
     String taxNumber,
     Uint8List? image,
-  ) {
+  ) async {
     String res = 'Some Error Occured';
     try {
       if (businessName.isNotEmpty &&
@@ -49,4 +69,6 @@ class VendorController {
     }
     return res;
   }
+
+  // FUNCTION TO SAVE VENDOR DATA ENDS HERE
 }
