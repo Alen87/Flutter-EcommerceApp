ab2261213e6004cfa8fd2643e83024acc5fd0180
Alen Boric <boricalen@live.com>
2023-02-23 00:52:42 +0100

    FromJson

--- vendor_only/lib/vendor/models/vendor_user_models.dart
+++ vendor_only/lib/vendor/models/vendor_user_models.dart
@@ -1,25 +1,39 @@
 class VendorUserModel {
-  final bool approved;
-  final String businessName;
-  final String cityValue;
-  final String countryValue;
-  final String email;
-  final String phoneNumber;
-  final String stateValue;
-  final String storeImage;
-  final String taxNumber;
-  final String taxRegistered;
+  final bool? approved;
+  final String? businessName;
+  final String? cityValue;
+  final String? countryValue;
+  final String? email;
+  final String? phoneNumber;
+  final String? stateValue;
+  final String? storeImage;
+  final String? taxNumber;
+  final String? taxRegistered;
 
-  VendorUserModel(
-    this.approved,
-    this.businessName,
-    this.cityValue,
-    this.countryValue,
-    this.email,
-    this.phoneNumber,
-    this.stateValue,
-    this.storeImage,
-    this.taxNumber,
-    this.taxRegistered,
-  );
+  VendorUserModel({
+    required this.approved,
+    required this.businessName,
+    required this.cityValue,
+    required this.countryValue,
+    required this.email,
+    required this.phoneNumber,
+    required this.stateValue,
+    required this.storeImage,
+    required this.taxNumber,
+    required this.taxRegistered,
+  });
+
+  VendorUserModel.fromJson(Map<String, Object> json)
+      : this(
+          approved: json['approved']! as bool,
+          businessName: json['businessName']! as String,
+          cityValue: json['cityValue']! as String,
+          countryValue: json['countryValue']! as String,
+          email: json['email']! as String,
+          phoneNumber: json['phoneNumber']! as String,
+          stateValue: json['stateValue']! as String,
+          storeImage: json['storeImage']! as String,
+          taxNumber: json['taxNumber']! as String,
+          taxRegistered: json!['taxRegistered']! as String,
+        );
 }
