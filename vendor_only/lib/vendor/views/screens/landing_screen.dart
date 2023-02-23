3c2e3d9e64178a16bee068cfda941a47d06c37af
Alen Boric <boricalen@live.com>
2023-02-24 00:35:20 +0100

    StreamBuilder

--- vendor_only/lib/vendor/views/screens/landing_screen.dart
+++ vendor_only/lib/vendor/views/screens/landing_screen.dart
@@ -1,13 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:vendor_only/vendor/models/vendor_user_models.dart';
 
 class LandingScreen extends StatelessWidget {
   const LandingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    final CollectionReference _vendorsStream =
+        FirebaseFirestore.instance.collection('vendors');
+
     return Scaffold(
-      body: Center(
-        child: Text('Landing Screen'),
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: _vendorsStream.doc(_auth.currentUser!.uid).snapshots(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text("Loading");
+          }
+          VendorUserModel vendorUserModel = VendorUserModel.fromJson(
+              snapshot.data!.data() as Map<String, dynamic>);
+          return Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(vendorUserModel.businessName.toString()),
+            ],
+          ));
+        },
       ),
     );
   }
