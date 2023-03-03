04de4569b44ade3e85c9db5e524ecaa5f75807b8
Alen Boric <boricalen@live.com>
2023-03-04 00:34:57 +0100

    Fetch Categories

--- multi_store/lib/vendor/views/screens/upload_tab_screens/general_screen.dart
+++ multi_store/lib/vendor/views/screens/upload_tab_screens/general_screen.dart
@@ -1,7 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class GeneralScreen extends StatelessWidget {
-  const GeneralScreen({Key? key}) : super(key: key);
+class GeneralScreen extends StatefulWidget {
+  @override
+  State<GeneralScreen> createState() => _GeneralScreenState();
+}
+
+class _GeneralScreenState extends State<GeneralScreen> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final List<String> _categoryList = [];
+
+  _getCategories() async {
+    return _firestore
+        .collection('categories')
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        _categoryList.add(doc['categoryName']);
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    _getCategories();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
